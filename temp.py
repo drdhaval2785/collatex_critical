6f6b33bd3d4d877d2c466a088a61bf031979fb1e
Dhaval Patel <drdhaval2785@gmail.com>
2025-09-25 22:06:43 +0530

    lorem ipsum

--- temp.py
+++ temp.py
@@ -1,17 +1,23 @@
 from collatex import *
 from collatex.core_classes import AlignmentTable, create_table_visualization
+import glob
 
-collation = Collation()
-collation.add_plain_witness("A"," BUpAla iti . BUpAlapramuKAH . BUmAn iti paryantA pati zaw rAjYo nAmavAcakAH . itiSabdaH prakArArTastena BUpAdayo'pi .. ..")
-collation.add_plain_witness("B", "BUpAla iti . BUpAlapramuKAH BUmAn iti paryantA ete zaw rAjYo nAmavAcakAH . itiSabdaH prakArArTastena BUpAdayo'pi .. ")
-collation.add_plain_witness("C","BUpAla iti BUpAlapramuKAH BUmAn iti paryantA gati zaw rAjYo nAmavAcakAH . itiSabdaprakArTastena BUpAdayo'pi .. 4 ..")
-#table = collate(collation, segmentation=False, near_match=True)
-#tei = collate(collation, output="tei", segmentation=False, near_match=True)
-#json = collate(collation, output="json", segmentation=False, near_match=True)
-#print(json)
-#print(tei)
-xml = collate(collation, output="xml")
-print(xml)
+if __name__ == "__main__":
+	collation = Collation()
+	directory = 'witnesses/lorem_ipsum/'
+	files = glob.glob(directory + '*.txt')
+	for fil in files:
+		bare = fil.rstrip('.txt').lstrip(directory)
+		witness_id = bare
+		witness_data = open(fil, 'r', encoding='utf-8').read()
+		collation.add_plain_witness(witness_id, witness_data)
+	#table = collate(collation, segmentation=False, near_match=True)
+	#tei = collate(collation, output="tei", segmentation=False, near_match=True)
+	#json = collate(collation, output="json", segmentation=False, near_match=True)
+	#print(json)
+	#print(tei)
+	xml = collate(collation, output="xml")
+	print(xml)
 
-table = collate(collation, )
-print(table)
+	table = collate(collation, )
+	print(table)
