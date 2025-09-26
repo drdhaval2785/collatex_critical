f133625526f517a494a33302fe7841dd7b41edc8
Dhaval Patel <drdhaval2785@gmail.com>
2025-09-26 21:45:19 +0530

    removed unnecessary

--- collator.py
+++ collator.py
@@ -3,7 +3,7 @@ from collatex.core_classes import AlignmentTable, create_table_visualization
 import glob
 import sys
 import os
-
+from datetime import datetime
 
 if __name__ == "__main__":
 	if len(sys.argv) != 2:
@@ -16,16 +16,22 @@ if __name__ == "__main__":
 	files = sorted(files)
 	collation = Collation()
 	print(files)
+	print('Witness addition started')
+	print(datetime.now())
 	for fil in files:
 		print(fil)
 		witness_id = os.path.splitext(os.path.basename(fil))[0]  # '1', '2', '3'
 		witness_data = open(fil, 'r', encoding='utf-8').read()
 		collation.add_plain_witness(witness_id, witness_data)
 		print(f"Added witness: {witness_id}")
+	print('Witness addition completed')
+	print(datetime.now())
+	print('Collation started')
 	xml = collate(collation, output="xml", segmentation=False, near_match=True)
 	fout = open(outfile, 'w', encoding='utf-8')
 	fout.write(xml)
 	#print(xml)
 	print(f"Collated witnesses and wrote to {outfile}")
+	print(datetime.now())
 	fout.close()
 
---
+++ readme.md
@@ -0,0 +1,11 @@
+# collatex-critical
+
+Ever wondered how to prepare critical editions with multiple witnesses?
+python-collatex provides a way to create collated XMLs, SVGs, HTMLs.
+But they are centered around showing differences in columns.
+
+For critical editors, the usual expectation is that the output should be in a PDF / markdown / HTML / latex file where the most frequent occurrence is shown in the main text and the variant readings of all other witnesses are duly noted in the footnote / endnote.
+
+If you want to do just the same, you are in the correct place.
+
+
