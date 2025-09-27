29f5afa8965ea743251be57671c151a3a69b4ca5
Dhaval Patel <drdhaval2785@gmail.com>
2025-09-27 10:04:12 +0530

    version 0.1.1 after handling paragraph style footnotes for compactness

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="collatex-critical",
-    version="0.1.0",
+    version="0.1.1",
     description="Critical edition enhancements for CollateX with footnote apparatus and transliterations",
     long_description=long_description,
     long_description_content_type="text/markdown",
